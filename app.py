03e105ce19340c1df7c25f64dbe5176ad9d141bf
Chomchaba Chunakiat <Chomchaba.Chanakiat@gmail.com>
2024-04-20 20:09:47 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,167 @@
+from flask import Flask, jsonify, request, Response
+from flask_socketio import SocketIO, join_room, leave_room
+from flask_jwt_extended import JWTManager, create_access_token, unset_access_cookies, jwt_required, get_jwt_identity
+from db import add_room_members, get_room, get_room_members, get_rooms_for_user, get_user, is_room_member, save_room, save_user
+from dotenv import load_dotenv
+from datetime import timedelta
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config['JWT_TOKEN_LOCATION'] = ['headers', 'cookies']
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=360)
+app.config['JWT_COOKIE_SECURE'] = True
+app.config['JWT_COOKIE_SAMESITE'] = 'None'
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+jwt = JWTManager(app)
+
+@app.route('/',methods=['GET'])
+@jwt_required() 
+def home():
+    current_username = get_jwt_identity()
+    rooms = get_rooms_for_user(current_username)
+    # Convert ObjectId to string for JSON serialization
+    formatted_rooms = []
+    for room in rooms:
+        room_id_str = str(room['_id']['room_id'])  
+        formatted_room = {
+            'room_id': room_id_str,
+            'room_name': room['room_name'],
+            'added_by': room['added_by'],
+            'added_at': room['added_at'].isoformat(),
+            'is_room_admin': room['is_room_admin']
+        }
+        formatted_rooms.append(formatted_room)
+    return jsonify({'username': current_username, 'rooms':formatted_rooms}), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password_input = data.get('password')
+
+    if not username or not password_input:
+        return jsonify({'error': 'Invalid credentials'}), 400
+    
+    try:
+        user = get_user(username)
+        if user and user.check_password(password_input):
+            access_token = create_access_token(identity=user.get_id())
+            res = jsonify(accessToken=access_token, username=user.get_id())
+            return res
+        else:
+            return jsonify({'error': 'Failed to login'}), 401
+    except Exception as e:
+        # Log the error for debugging
+        app.logger.error(f"Error occurred while fetching user '{username}' from database: {str(e)}")
+        return jsonify({'error': 'Internal server error'}), 500
+
+@app.route('/logout', methods=['POST'])   
+@jwt_required() 
+def logout():
+    print('Received POST request for /logout')
+    res = Response(status=204)
+    unset_access_cookies(res)
+    return res
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data.get('username')
+    password_input = data.get('password')
+
+    if not username or not password_input:
+        return jsonify({'error': 'Invalid credentials'}), 400
+    
+    user = get_user(username)
+    if user:
+        return jsonify({'error': 'This username has been used!'}), 400    
+        
+    else:
+        save_user(username, password_input)
+        access_token = create_access_token(identity=username)
+        res = jsonify(accessToken=access_token, username = username)
+        return res
+
+
+@app.route('/create-room', methods=['GET','POST'])
+@jwt_required()
+def create_room():
+    current_username = get_jwt_identity()
+    if request.method == 'GET':
+        return jsonify({'username': current_username}), 200
+    else:
+        room_name = request.get_json().get('room_name')
+        usernames = [username.strip() for username in request.get_json().get('members').split(',')]
+        if len(room_name) and len(usernames):
+            room_id = save_room(room_name, current_username)
+            if current_username in usernames:
+                usernames.remove(current_username)
+            if len(usernames) > 1 or usernames[0] != '':
+                add_room_members(room_id, room_name, usernames, current_username)
+            return jsonify(room_id=str(room_id))
+        else:
+            return jsonify({'error': 'Invalid credentials'}), 400
+           
+@app.route('/rooms/<room_id>/')
+@jwt_required()
+def view_room(room_id):
+    current_username = get_jwt_identity()
+    room = get_room(room_id)
+    if room and is_room_member(room_id, current_username):
+        room_id_str = str(room['_id'])
+        formatted_room = {
+            '_id' : room_id_str,
+            'name': room['name'],
+            'created_by': room['created_by'],
+            'created_at' : room['created_at'].isoformat()
+        }
+        room_members = get_room_members(room_id)
+        formatted_room_members = []
+        for room_member in room_members:
+            room_id_str = str(room_member['_id']['room_id'])  
+            formatted_room_member = {
+                'room_id': room_id_str,
+                'username': room_member['_id']['username'],
+                'added_by': room_member['added_by'],
+                'added_at': room_member['added_at'].isoformat(),
+                'is_room_admin': room_member['is_room_admin']
+            }
+            formatted_room_members.append(formatted_room_member)
+        return jsonify(username=current_username, room=formatted_room, room_members=formatted_room_members)
+    else :
+        return jsonify({'error': 'Invalid credentials'}), 400
+
+
+##############################################################    
+# socket programming...
+##############################################################
+
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    app.logger.info("{} has sent message to the room {}: {}".format(data['username'],
+                                                                    data['room'],
+                                                                    data['message']))
+    socketio.emit('receive_message', data, room=data['room'])
+
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
+    join_room(data['room'])
+    socketio.emit('join_room_announcement', data, room=data['room'])
+
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    app.logger.info("{} has left the room {}".format(data['username'], data['room']))
+    leave_room(data['room'])
+    socketio.emit('leave_room_announcement', data, room=data['room'])
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=5050)
---
+++ db.py
@@ -0,0 +1,73 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from werkzeug.security import generate_password_hash
+from user import User
+from datetime import datetime
+from bson import ObjectId
+import os
+
+mongo_uri = os.getenv("MONGO_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(mongo_uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+chat_db = client.get_database('chat-db')
+users_collection = chat_db.get_collection("users")
+rooms_collection = chat_db.get_collection("rooms")
+room_members_collection = chat_db.get_collection("room_members")
+
+def save_user(username, password):
+    password_hash = generate_password_hash(password)
+    users_collection.insert_one({'_id':username, 'password':password_hash})
+
+def get_user(username):
+    user_data = users_collection.find_one({'_id':username})
+    return User(user_data['_id'], user_data['password']) if user_data else None
+
+def save_room(room_name, created_by):
+    room_id = rooms_collection.insert_one(
+        {'name': room_name, 'created_by': created_by, 'created_at': datetime.now()}).inserted_id
+    add_room_member(room_id, room_name, created_by, created_by, is_room_admin=True)
+    return room_id
+
+def update_room(room_id, room_name):
+    rooms_collection.update_one({'_id': ObjectId(room_id)}, {'$set': {'name': room_name}})
+    room_members_collection.update_many({'_id.room_id': ObjectId(room_id)}, {'$set': {'room_name': room_name}})
+
+def get_room(room_id):
+    return rooms_collection.find_one({'_id': ObjectId(room_id)})
+
+def add_room_member(room_id, room_name, username, added_by, is_room_admin=False):
+    room_members_collection.insert_one(
+        {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
+         'added_at': datetime.now(), 'is_room_admin': is_room_admin})
+    
+def add_room_members(room_id, room_name, usernames, added_by):
+    room_members_collection.insert_many(
+        [{'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
+          'added_at': datetime.now(), 'is_room_admin': False} for username in usernames])
+    
+def remove_room_members(room_id, usernames):
+    room_members_collection.delete_many(
+        {'_id': {'$in': [{'room_id': ObjectId(room_id), 'username': username} for username in usernames]}})
+
+
+def get_room_members(room_id):
+    return list(room_members_collection.find({'_id.room_id': ObjectId(room_id)}))
+
+def get_rooms_for_user(username):
+    return list(room_members_collection.find({'_id.username': username}))
+
+
+def is_room_member(room_id, username):
+    return room_members_collection.count_documents({'_id': {'room_id': ObjectId(room_id), 'username': username}})
+
+def is_room_admin(room_id, username):
+    return room_members_collection.count_documents(
+        {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'is_room_admin': True})
