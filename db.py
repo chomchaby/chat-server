9ed6866c4bb64e6618c981d9842c287e1cd49f6e
nathathai <nathathaiaungudomsin@gmail.com>
2024-04-21 20:17:00 +0700

    chat room

--- db.py
+++ db.py
@@ -27,6 +27,9 @@ room_members_collection = chat_db.get_collection("room_members")
 group_messages = chat_db.get_collection("group_messages")
 direct_messages = chat_db.get_collection("direct_messages")
 
+chat_room_collection = chat_db.get_collection("chat_room")
+
+# User Operation--------------------------------------------------------------------------------------------------------
 def save_user(username, password):
     password_hash = generate_password_hash(password)
     users_collection.insert_one({'_id':username, 'password':password_hash})
@@ -35,9 +38,13 @@ def get_user(username):
     user_data = users_collection.find_one({'_id':username})
     return User(user_data['_id'], user_data['password']) if user_data else None
 
-def save_room(room_name, created_by):
+# Room Operation --------------------------------------------------------------------------------------------------------
+def save_room(room_name, room_type, created_by):
     room_id = rooms_collection.insert_one(
-        {'name': room_name, 'created_by': created_by, 'created_at': datetime.now()}).inserted_id
+        {'name': room_name,
+         'type': room_type, #(Direct, PublicGroup, PrivateGroup)
+         'created_by': created_by,
+         'created_at': datetime.now()}).inserted_id
     add_room_member(room_id, room_name, created_by, created_by, is_room_admin=True)
     return room_id
 
@@ -48,20 +55,55 @@ def update_room(room_id, room_name):
 def get_room(room_id):
     return rooms_collection.find_one({'_id': ObjectId(room_id)})
 
+# Room Member Operation -------------------------------------------------------------------------------------------------
 def add_room_member(room_id, room_name, username, added_by, is_room_admin=False):
-    room_members_collection.insert_one(
-        {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
-         'added_at': datetime.now(), 'is_room_admin': is_room_admin})
+    # Check if the user exists in the system
+    if users_collection.count_documents({"_id": username}) == 0:
+        raise ValueError("User '{}' does not exist in the system.".format(username))
     
-def add_room_members(room_id, room_name, usernames, added_by):
-    room_members_collection.insert_many(
-        [{'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
-          'added_at': datetime.now(), 'is_room_admin': False} for username in usernames])
+    # Check if the user is already a member of the group
+    if is_room_member(room_id, username):
+        raise ValueError("User '{}' is already a member of the group.".format(username))
     
-def remove_room_members(room_id, usernames):
-    room_members_collection.delete_many(
-        {'_id': {'$in': [{'room_id': ObjectId(room_id), 'username': username} for username in usernames]}})
+    room_members_collection.insert_one({
+        '_id': {'room_id': ObjectId(room_id), 'username': username}, 
+        'room_name': room_name, 
+        'added_by': added_by,
+        'added_at': datetime.now(), 
+        'is_room_admin': is_room_admin
+    })
 
+def add_room_members(room_id, room_name, usernames, added_by):
+    for username in usernames:
+        # Check if the user exists in the system
+        if users_collection.count_documents({"_id": username}) == 0:
+            raise ValueError("User '{}' does not exist in the system.".format(username))
+        
+        # Check if the user is already a member of the group
+        if is_room_member(room_id, username):
+            raise ValueError("User '{}' is already a member of the group.".format(username))
+    
+    bulk_operations = [
+        {
+            '_id': {'room_id': ObjectId(room_id), 'username': username},
+            'room_name': room_name,
+            'added_by': added_by,
+            'added_at': datetime.now(),
+            'is_room_admin': False
+        }
+        for username in usernames
+    ]
+    room_members_collection.insert_many(bulk_operations)
+
+def remove_room_members(room_id, usernames):
+    for username in usernames:
+        # Check if the user is a member of the group
+        if not is_room_member(room_id, username):
+            raise ValueError("User '{}' is not a member of the group.".format(username))
+    
+    room_members_collection.delete_many({
+        '_id': {'$in': [{'room_id': ObjectId(room_id), 'username': username} for username in usernames]}
+    })
 
 def get_room_members(room_id):
     return list(room_members_collection.find({'_id.room_id': ObjectId(room_id)}))
@@ -76,3 +118,5 @@ def is_room_member(room_id, username):
 def is_room_admin(room_id, username):
     return room_members_collection.count_documents(
         {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'is_room_admin': True})
+
+# 
