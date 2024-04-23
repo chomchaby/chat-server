2a336e015ae26a096c5f1d2ef1a1b57687ac2220
Chomchaba Chunakiat <Chomchaba.Chanakiat@gmail.com>
2024-04-23 17:55:13 +0700

    set logout status code back to 204

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def login():
 @jwt_required() 
 def logout():
     print('Received POST request for /logout')
-    res = Response(status=200)
+    res = Response(status=204)
     unset_access_cookies(res)
     return res
 
